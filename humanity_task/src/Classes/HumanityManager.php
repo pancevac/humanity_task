836234df7cd1f79e73dd44d8266675984d47ce3e
pancevac <sinisa.buncic7@gmail.com>
2019-12-25 12:27:34 +0100

    Added comments.

--- humanity_task/src/Classes/HumanityManager.php
+++ humanity_task/src/Classes/HumanityManager.php
@@ -8,13 +8,26 @@ use App\Api\GenericHumanity;
 
 class HumanityManager
 {
+    /**
+     * @var GenericHumanity
+     */
     protected $humanityApi;
 
+    /**
+     * @param GenericHumanity $humanityApi
+     */
     public function __construct(GenericHumanity $humanityApi)
     {
         $this->humanityApi = $humanityApi;
     }
 
+    /**
+     * Return array with shifts objects between given time intervals.
+     *
+     * @param \DateTime $startTime
+     * @param \DateTime $endTime
+     * @return array
+     */
     public function getShifts(\DateTime $startTime, \DateTime $endTime): array
     {
         return $this->humanityApi->getShifts($startTime, $endTime);
